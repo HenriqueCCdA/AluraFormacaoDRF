bdffb4ffa2adbd0bc1fa7c1b4dba5133484f5388
Henrique Andrade <henrique.ccda@gmail.com>
2022-10-02 00:58:02 -0300

    m

--- django_rest_framework_curso1/setup/urls.py
+++ django_rest_framework_curso1/setup/urls.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
-from django.urls import path
-from escola.views import alunos
+from django.urls import path, include
+from escola.views import (
+    AlunosViewSet,
+    CursosViewSet,
+    MatriculaViewSet,
+    ListaMatriculasAluno,
+    ListaAlunosMatriculados,
+)
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register("alunos", AlunosViewSet, basename="Alunos")
+router.register("cursos", CursosViewSet, basename="Cursos")
+router.register("matriculas", MatriculaViewSet, basename="Matriculas")
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('alunos/', alunos),
+    path("admin/", admin.site.urls),
+    path("", include(router.urls)),
+    path("alunos/<int:pk>/matriculas/", ListaMatriculasAluno.as_view()),
+    path("curso/<int:pk>/matriculas/", ListaAlunosMatriculados.as_view()),
 ]
